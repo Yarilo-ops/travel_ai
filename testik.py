e9e2fa423e404dd7b879114416f275d2ad659856
Ilya_Rodin <krokodildandy06@gmail.com>
2024-05-06 18:52:20 +0300

    initional

---
+++ testik.py
@@ -0,0 +1,22 @@
+idf = "NDBhNDQ2MmItN2U3NC00N2E0LTkzYjAtMzY5MzQyZjA3M2U1OjM4ZGMwOWEzLTIwOTEtNDBmZi1hNjVjLWUwMjRmMTZhYjNhNQ=="
+"""Пример работы с чатом через gigachain"""
+from langchain.schema import HumanMessage, SystemMessage
+from langchain.chat_models.gigachat import GigaChat
+
+# Авторизация в сервисе GigaChat
+chat = GigaChat(credentials=f'{idf}', verify_ssl_certs=False)
+
+messages = [
+    SystemMessage(
+        content="Ты эмпатичный бот-психолог, который помогает пользователю решить его проблемы."
+    )
+]
+
+while(True):
+    # Ввод пользователя
+    user_input = input("User: ")
+    messages.append(HumanMessage(content=user_input))
+    res = chat(messages)
+    messages.append(res)
+    # Ответ модели
+    print("Bot: ", res.content)
