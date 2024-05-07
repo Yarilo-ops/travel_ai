f0739ecd237d03e0497b73e40d72f7e9e2ef64b5
Yaroslav <yrikkonovalov91@gmail.com>
2024-05-07 12:37:27 +0300

    make route

--- gis.py
+++ gis.py
@@ -1,27 +1,28 @@
-import json
 import requests
+import settings
+
 
 def PublicTransport(url):
     d = {
         "locale": "ru",
         "source":
-        {
-            "name": "Point A",
-            "point":
             {
-                "lat": 51.734588,
-                "lon": 36.149328
-            }
-        },
+                "name": "Point A",
+                "point":
+                    {
+                        "lat": 51.734588,
+                        "lon": 36.149328
+                    }
+            },
         "target":
-        {
-            "name": "Point B",
-            "point":
             {
-                "lat": 51.734183,
-                "lon": 36.176865
-            }
-        },
+                "name": "Point B",
+                "point":
+                    {
+                        "lat": 51.734183,
+                        "lon": 36.176865
+                    }
+            },
         "transport": ["bus", "tram"]
     }
     headers = {
@@ -29,3 +30,19 @@ def PublicTransport(url):
     }
     resp = requests.post(url, json=d, headers=headers)
     return resp
+
+
+def getSight(x, y):
+    url = settings.getSights + f"q=Достопримечательность&sort_point={x},{y}&key={settings.token}"
+    try:
+        response = requests.get(url)
+        return response.json()
+    except requests.exceptions.InvalidSchema:
+        return {"status": "400"}
+
+
+def getRoute(args, way="multimodal"):
+    url = f"https://2gis.ru/directions/points/"
+    for val in args:
+        url += f"{val[0]}%2C{val[1]}%3B{val[2]}" + "%7C"
+    print(url)
--- settings.py
+++ settings.py
@@ -1,1 +1,4 @@
 gis_url = "https://routing.api.2gis.com/public_transport/2.0?key=d6eac903-d3d2-45f0-8345-981f03a9c39a"
+token = "e54ece61-706e-4274-9daa-51a572cf8c7c"
+postPublic = f"https://routing.api.2gis.com/public_transport/2.0?key={token}"
+getSights = f"https://catalog.api.2gis.com/3.0/items?"
